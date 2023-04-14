f127cf24183c4c9e0216c9d995e05660cc082447
brichards64Â <benjamin.richards@warwick.ac.uk>
2023-04-14 10:52:37 +0000

    added slowcontrol receiver to slow control collection to negate the need for tools

--- DataModel/SlowControlCollection.h
+++ DataModel/SlowControlCollection.h
@@ -2,6 +2,23 @@
 #define SLOW_CONTROL_COLLECTION_H
 
 #include <SlowControlElement.h>
+#include <zmq.hpp>
+#include "DAQUtilities.h"
+
+class SlowControlCollection;
+
+struct SlowControlCollectionThread_args:Thread_args{
+
+  SlowControlCollectionThread_args();
+  ~SlowControlCollectionThread_args();
+
+  zmq::socket_t* sock;
+  zmq::pollitem_t items[1];
+  int poll_length;
+
+  SlowControlCollection* SCC;
+  
+};
 
 class SlowControlCollection{
 
@@ -10,6 +27,9 @@ class SlowControlCollection{
   SlowControlCollection();
   ~SlowControlCollection();
 
+  bool Init(zmq::context_t* context, int port=555);
+  bool ListenForData(int poll_length=0);
+  bool InitThreadedReceiver(zmq::context_t* context, int port=555, int poll_length=100);
   SlowControlElement* operator[](std::string key);
   bool Add(std::string name, SlowControlElementType type);
   bool Remove(std::string name);
@@ -19,6 +39,12 @@ class SlowControlCollection{
  private:
 
   std::map<std::string, SlowControlElement*> SC_vars;
+  
+  DAQUtilities* m_util;
+  zmq::context_t* m_context;
+  SlowControlCollectionThread_args* args;
+
+  static void Thread(Thread_args* arg);
 
 };
 
