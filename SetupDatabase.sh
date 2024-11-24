1c0b066a29c4a9afc0af1bc4ae5812217f865649
Chidi Ekuma <7931750+ekumachidi@users.noreply.github.com>
2024-11-24 15:52:28 +0000

    Implement database backed authentication
    Currently, the login is not validated, it simply redirects user to the homepage, this fix introduces a database backed login functionality with the following updates:
    
    * Extract javascript from login.html to auth.js for modularity
    * Redesign the login form
    * Adds `postgresql-contrib` for additional postgres packages needed for password encryption
    * Adds `pgcrypto` extension to the `db`
    * Creates a default user in db
    * Validates the login credentials against a hashed password value rather than the plain text previously stored

--- SetupDatabase.sh
+++ SetupDatabase.sh
@@ -18,6 +18,9 @@ sudo -u postgres createuser -s root
 echo "creating 'daq' database"
 sudo -u postgres psql -c "create database daq with owner=root;"
 
+echo "creating pgcrypto extension"
+psql -ddaq -c "CREATE EXTENSION IF NOT EXISTS pgcrypto;"
+
 echo "creating monitoring table"
 psql -ddaq -c "create table monitoring (time timestamp with time zone NOT NULL, device text NOT NULL, data JSONB NOT NULL);"
 
@@ -59,5 +62,9 @@ psql -ddaq -c "create trigger plots_update before update on plots for row execut
 
 #echo "registering database to start on boot"
 #echo " sudo -u postgres /usr/bin/pg_ctl start -D /var/lib/pgsql/data -s -o \"-p 5432\" -w -t 300;" >> /etc/rc.local
+
+# Insert a default user for testing
+echo "Inserting a default user"
+psql -ddaq -c "INSERT INTO users (username, password_hash) VALUES ('dev_user', crypt('dev_password', gen_salt('bf')));"
 
 touch /.DBSetupDone
