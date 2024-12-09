4422d337255d3aa389af1662df2bb3e32fd24d3b
Ekuma Chidi <7931750+ekumachidi@users.noreply.github.com>
2024-12-09 12:32:34 +0000

    Propagate changes from fork to parent repo (#39)
    * Remove html-StandAlone/ and rename html-StandAlone2/ to html-StandAlone/
    
    * Propagate changes from wcte fork to parent repo
    
    * Remove wcte specific navigation links
    
    * * Add material.min.js.map symlinked to Standalone and Detector
    * Remove the emacs backup file html-Detector/#login.html#
    * Move stylesheets directory to html-Common
    * Remove html-Detector/Testing
    * Remove html-Detector/Config/test
    
    * Remove `ServiceSend.setsockopt(ZMQ_IMMEDIATE, b);` in `cgi-bin/sendcommand2nopadding.cpp` as that turned out to be a mistake
    
    * More updates
    
    * Move monitoring plots dir to html-Common and add symlinks
    
    * Revert IP change
    
    * Rename logs to Logs
    
    * fix stylesheets and symlinks
    
    ---------
    
    Co-authored-by: marcus o'flaherty <marc@u7722.net>

--- cgi-bin/sendcommand2nopadding.cpp
+++ cgi-bin/sendcommand2nopadding.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include <string>
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <sstream>
 #include <fstream>
 
-#include <cgicc/CgiDefs.h> 
-#include <cgicc/Cgicc.h> 
-#include <cgicc/HTTPHTMLHeader.h> 
+#include <cgicc/CgiDefs.h>
+#include <cgicc/Cgicc.h>
+#include <cgicc/HTTPHTMLHeader.h>
 #include <cgicc/HTMLClasses.h>
 
 #include <Store.h>
@@ -27,53 +27,53 @@ using namespace cgicc;
 using namespace ToolFramework;
 
 int main (){
-	
+
 	//boost::uuids::uuid m_UUID=boost::uuids::random_generator()();
 	//long msg_id=0;
-	
+
 	std::cout <<"Content-type:text/html\r\n\r\n";
-	std::cout<<"<html><body>"<<std::endl; 
-	
+	std::cout<<"<html><body>"<<std::endl;
+
 	// debug file
 	std::ofstream* debugf = nullptr;
-	
+
 	try {
-		
+
 		// uncomment to enable debug output
 		//debugf = new std::ofstream("/tmp/sendcommand2nopadding.log",std::ios::out);
-		
-		
+
+
 		if(debugf) *debugf << "opening zmq context" << std::endl;
 		zmq::context_t *context=new zmq::context_t(1);
-		
-		
+
+
 		///////////////response /////////////////
 		Cgicc formData;
-		
+
 		std::string IP;
 		std::string port;
 		std::string command;
 		std::string var1;
 		std::stringstream response;
-		
+
 		form_iterator fi = formData.getElement("ip");
 		if( !fi->isEmpty() && fi != (*formData).end()) {
 			IP= **fi;
 			if(debugf) *debugf << "IP: " << IP <<std::endl;
-			
+
 			form_iterator pi = formData.getElement("port");
 			if( !pi->isEmpty() && pi != (*formData).end()) {
 				port= **pi;
 				if(debugf) *debugf << "port: " << port <<std::endl;
-				
+
 				form_iterator ti = formData.getElement("command");
 				if( !ti->isEmpty() && ti != (*formData).end()) {
 					command= **ti;
 					if(debugf) *debugf << "command: '" << command << "'" <<std::endl;
-					
+
 					std::stringstream ss(command);
 					//ss >> command >> var1;
-					
+
 					ss >> command;
 					getline(ss, var1);
 					if(var1.length()>0){
@@ -82,62 +82,61 @@ int main (){
 					//std::cout<<"comand="<<command<<" : var1="<<var1<<std::endl;
 					//tmp >> command>> std::ws;
 					//getline(tmp, var1);
-					
+
 					boost::posix_time::ptime t = boost::posix_time::microsec_clock::universal_time();
 					std::stringstream isot;
 					isot<<boost::posix_time::to_iso_extended_string(t) << "Z";
 					Store bb;
-					
+
 					//bb.Set("uuid",m_UUID);
 					//bb.Set("msg_id",msg_id);
 					bb.Set("msg_time", isot.str());
 					bb.Set("msg_type", "Command");
 					bb.Set("msg_value", command);
 					bb.Set("var1",var1);
-					
+
 					std::string tmp="";
 					bb>>tmp;
-					
+
 					if(debugf) *debugf << "creating socket" << std::endl;
 					zmq::socket_t ServiceSend (*context, ZMQ_REQ);
-					
+
 					if(debugf) *debugf << "setting socket options" << std::endl;
 					int a=5000;
 					int b=1;
 					ServiceSend.setsockopt(ZMQ_RCVTIMEO, a);
 					ServiceSend.setsockopt(ZMQ_SNDTIMEO, a);
-					ServiceSend.setsockopt(ZMQ_IMMEDIATE, b);
-					
+
 					if(debugf) *debugf << "connecting to socket" << std::endl;
 					std::stringstream connection;
 					connection<<"tcp://"<<IP<<":"<<port;
 					ServiceSend.connect(connection.str().c_str());
-					
+
 					zmq::message_t send(tmp.length() + 1);
 					snprintf ((char *) send.data(), tmp.length() + 1 , "%s" ,tmp.c_str()) ;
-					
+
 					// FIXME should poll for listener before sending
 					if(debugf) *debugf << "sending '"<<tmp<<"'" << std::endl;
 					//response<<"Sending Command {"<<command<<"} to ["<<IP<<":"<<port<<"] <br>";
 					bool ok = ServiceSend.send(send);
 					if(debugf) *debugf << "send success: " << ok << std::endl;
 					if(!ok) throw std::runtime_error("zmq send failed");
-					
+
 					zmq::message_t receive;
 					if(debugf) *debugf << "receiving..." << std::endl;
 					// FIXME should poll for inbound before receiving
-					
+
 					ok = ServiceSend.recv(&receive);
 					if(debugf) *debugf << "recv success: " << ok << std::endl;
 					if(!ok) throw std::runtime_error("zmq rcv failed");
-					
+
 					// FIXME this is not a safe way to retrieve the result, use memcpy
 					std::istringstream iss(static_cast<char*>(receive.data()));
 					std::string answer=iss.str();
-					
+
 					if(debugf) *debugf << "got response: '"<<answer<<"'" << std::endl;
 					if(answer=="") throw std::runtime_error("zmq rcv got empty response");
-					
+
 					Store rr;
 					rr.JsonParser(answer);
 					//std::cout<<"answer="<<answer<<" : ";
@@ -146,40 +145,40 @@ int main (){
 					} else {
 						if(debugf) *debugf << "no 'msg_type' or 'msg_type' not 'Command Reply' in response" << std::endl;
 					}
-					
+
 				} else {
 					// else response<< "Service does not exist"<<std::endl<<std::endl;
 					if(debugf) *debugf << "no command specified" << std::endl;
-					
+
 				}
-				
+
 			} else {
 				//	response << "No Command Entered " << endl;
 				if(debugf) *debugf << "no port specified" << std::endl;
 			}
-			
+
 		} else {
 			// response << "Cannot Send Command No Services Found" << endl;
 			if(debugf) *debugf << "no ip specified" << std::endl;
-			
+
 		}
-		
+
 		if(debugf) *debugf << "got repsonse '"<<response.str()<<"'" << std::endl;
 		//std::cout<<"hi ben";
 		std::cout<<response.str()<<endl;
-		
+
 	} catch(std::exception& e){
 		// surely this is useful, though perhaps it will break things that expect a specific format...
 		//std::cout<<e.what()<<std::endl;
 	}
-	
+
 	if(debugf){
 		debugf->close();
 		delete debugf;
-	} 
-	
-	cout<<"<body/><html/>"; 
-	
-	 
+	}
+
+	cout<<"<body/><html/>";
+
+
 	return 0;
 }
--- html-Detector/Alarms/alarms.js
+++ html-Detector/Alarms/alarms.js
@@ -1,44 +1,130 @@
+"use strict";
 import { GetPSQLTable } from "/includes/functions.js";
 
-var alarm_table = document.getElementById("alarm output");
-var alarmsinterval = setInterval(GetAlarms, 5000);
+var alarm_table;
+var alarmsinterval;
 var checking_alarm_table = false;
+var oldest;
+var newest;
 
-GetAlarms();
+ if (document.readyState !== 'loading'){
+ 	Init();
+ } else {
+ 	document.addEventListener("DOMContentLoaded", Init);
+ }
 
-function GetAlarms(){
-    
-    if(checking_alarm_table) return;
-    checking_alarm_table=true;
+function Init(){
+	alarm_table = document.getElementById("alarm_output");
+	document.getElementById("btnSilenceAll").addEventListener('click', SilenceByDeviceName);
+	document.getElementById("loadMore").addEventListener('click', () => { GetAlarms(true); });
+	document.getElementById("deviceNameFilter").addEventListener('change', () => { alarm_table.innerHTML = ""; GetAlarms(); } );
+	GetAlarms();
+	alarmsinterval = setInterval(GetAlarms, 5000);
+}
 
-    GetPSQLTable("select * from alarms order by time desc limit 50", "root", "daq", true).then(function(result){
-	
-	alarm_table.innerHTML="";
-	alarm_table.innerHTML=result;
-	
-	const rows = alarm_table.getElementsByTagName("tr");
-	
-	for (const row of rows) {
-	    if(row.rowIndex==0) continue;
-	    const newCell = document.createElement("td");
-	    const btnID = "btnSilence_"+(row.rowIndex-1); // optional
-	    const timeval = row.cells[0].innerText;
-	    newCell.innerHTML = `<button timeval=\"${timeval}\", id=\"${btnID}\">Silence</button>`;
-	    row.appendChild(newCell);
-	    // either of the following
-	    document.getElementById(btnID).addEventListener("click", () => { Silence(timeval); });
-	    //document.getElementById(btnID).onclick = function(){ Silence(timeval); };
+
+function GetAlarms(arg=false){
+	// note that arg may be an event if function is attached as callback
+	// so just because we specify a default boolean false, if its called as a callback with no args, it'll not match (!arg)
+	let append = false;
+	if(typeof(arg) == 'boolean'){
+		append = arg;
 	}
-	checking_alarm_table=false;
 	
-    });
-    
+	if(checking_alarm_table) return;
+	checking_alarm_table=true;
+	
+	//console.log(`GetAlarms called with append ${append}`);
+	
+	try {
+		
+		const devName = document.getElementById("deviceNameFilter").value;
+		let nameFilter1 = "";
+		let nameFilter2 = "";
+		if(devName!=""){
+			nameFilter1 = `WHERE device='${devName}'`;
+			nameFilter2 = `AND device='${devName}'`;
+		}
+		
+		const nrows = document.getElementById("numrows").value;
+		let tablesize = alarm_table.getElementsByTagName("tr").length;
+		if(tablesize>0) tablesize--; // skip header row
+		if(tablesize==0 || typeof(oldest) == 'undefined') append=false; // can't append if it's empty
+		
+		let query;
+		// update status of all alarms shown
+		if(append){
+			query = `select * from alarms WHERE time<'${oldest}' ${nameFilter2} order by time desc limit ${nrows}`;
+		} else {
+			const rowstoget = (tablesize==0) ? nrows : tablesize;
+			query = `select * from alarms ${nameFilter1} order by time desc limit ${rowstoget}`;
+		}
+		//console.log(`runnin query '${query}'`);
+		
+		GetPSQLTable(query, "root", "daq", true).then(function(result){
+			
+			let rows;
+			let temp_table;
+			if(!append){
+				alarm_table.innerHTML = result;
+				rows = alarm_table.getElementsByTagName("tr");
+			} else {
+				temp_table = document.createElement("table");
+				temp_table.innerHTML = result;
+				rows = temp_table.getElementsByTagName("tr");
+			}
+			
+			oldest = rows[rows.length - 1].cells[0].innerHTML;
+			
+			for (const row of rows) {
+				if(row.rowIndex==0) continue; // skip header row
+				const newCell = document.createElement("td");
+				let btnNum = row.rowIndex-1;
+				if(append) btnNum += tablesize;
+				const btnID = "btnSilence_"+btnNum; // optional
+				const timeval = row.cells[0].innerText;
+				newCell.innerHTML = `<button timeval=\"${timeval}\", id=\"${btnID}\">Silence</button>`;
+				row.appendChild(newCell);
+				if(append){
+					alarm_table.getElementsByTagName('tbody')[0].insertAdjacentHTML('beforeend', row.innerHTML);
+					
+					//let newrow = alarm_table.insertRow();
+					//for (let cell of row.cells) newrow.insertCell().innerText = cell.innerText;
+				}
+				// either of the following
+				document.getElementById(btnID).addEventListener("click", () => { Silence(timeval); });
+				//document.getElementById(btnID).onclick = function(){ Silence(timeval); };
+			}
+			
+			if(append) temp_table.remove;
+			
+			checking_alarm_table=false;
+			
+		});
+		
+	} catch(err){
+		console.error(err);
+		checking_alarm_table=false;
+	}
+	
+}
+
+
+
+function SilenceByDeviceName(){
+	const devName = document.getElementById("deviceNameSilence").value;
+	if(devName =="") return;
+	
+	GetPSQLTable(`update alarms set silenced=1 where device='${devName}'`, "root", "daq", false);
+	
+	// update alarms table
+	GetAlarms();
 }
 
 function Silence(time){
-  //console.log("Silence called by button "+event.target.id);
-  //console.log("time is: "+time);
-
-  GetPSQLTable(`update alarms set silenced=1 where time='${time}'`, "root", "daq", false);
-    GetAlarms();
+	//console.log("Silence called by button "+event.target.id);
+	//console.log("time is: "+time);
+	
+	GetPSQLTable(`update alarms set silenced=1 where time='${time}'`, "root", "daq", false);
+	GetAlarms();
 }
