cb6aca46d94ec449861975ac1321bc674995f204
Chidi Ekuma <7931750+ekumachidi@users.noreply.github.com>
2025-02-21 11:47:21 +0000

    Remove old implementation

--- html-Common/Logs/log_DB.js
+++ html-Common/Logs/log_DB.js
@@ -233,25 +233,6 @@ function FetchLogs() {
 		CheckLogDivs();
 
 		log_display.scrollTop = log_display.scrollHeight;
-		// let newLogs = [];
-		// result.forEach(log => {
-		// 	const logText = `${log.time} | Severity: ${log.severity} | Message: ${log.message}`;
-		// 	if (!existingLogs.includes(logText)) {
-		// 		newLogs.push(logText);
-		// 	}
-		// });
-
-		// if (newLogs.length > 0) {
-		// 	newLogs.forEach(logText => {
-		// 		const log_entry = document.createElement("p");
-		// 		log_entry.textContent = logText;
-		// 		log_display.appendChild(log_entry);
-		// 	});
-		// } else if (log_display_content.length === 0) {
-		// 	const noLogsMessage = document.createElement("p");
-		// 	noLogsMessage.textContent = "No logs available for this device.";
-		// 	log_display.appendChild(noLogsMessage);
-		// }
 	}).catch(function (error) {
 			console.error("Error fetching logs:", error);
 			alert("There was an error fetching the logs.");
@@ -349,76 +330,3 @@ function ExportLogs(device, log_display) {
 	link.click();
 	document.body.removeChild(link);
 }
-
-var log_output=document.getElementById("log_output");
-var update = setInterval(GetLogSources, 5000); // Run the GetLogSources() function every minute
-var updating = false;
-
-//load logs on startup
-GetLogSources();
-
-//generic funcion for returning SQL table
-function gettable(command){
-        return GetPSQLTable(command, 'root', 'daq', true);
-}
-
-function GetLogSources(){ //command to get log files, first cehckes which devices exist then gets the last 15 messages for each
-
-    if(updating) return;
-    updating = true;
-
-    var command="SELECT distinct(device) from logging"
-
-    gettable(command).then(function(result){
-
-	var table= document.createElement('table');
-	table.innerHTML = result;
-	var tmp=[];
-
-	for( var i=1; i < table.rows.length; i++){
-	    tmp[i-1] =table.rows[i].innerText.replace(/\s/g, '');
-	}
-
-	var counter=0;
-
-	tmp.map(function(row){
-
-	    var command= "select time, severity, message from logging where device='" + row + "' order by time desc limit 15;";
-	    gettable(command).then(function(result2){
-
-		var pos=0;
-		for( var k=0; k < tmp.length; k++){
-		    if(row==tmp[k]) pos=k;
-		}
-
-		tmp[pos]="<a href=\"/cgi-bin/sqlquerystring.cgi?method=post&user=root&db=daq&command=select time, severity, message from logging where device=%22" + row + "%22 order by time desc;\">" + row + "</a><div id=" + row + " align='left' style=\"#ccc;font:12px/13px Georgia, Garamond, Serif;overflow:scroll;border:2px solid;padding:1%;height:200px\">";
-		var output_table= document.createElement('table');
-		output_table.innerHTML=result2;
-
-		for( var j=1; j < output_table.rows.length; j++){
-		    tmp[pos]+="<b>" + output_table.rows[j].cells[0].innerText +" [" + output_table.rows[j].cells[1].innerText + "]:</b> " + output_table.rows[j].cells[2].innerText + "<br>"
-		}
-
-		tmp[pos]+= "</div><p> <script type=\"text/javascript\"> var divid = document.getElementById(\""+ row + "\"); divid.scrollTop = divid.scrollHeight; </script>"
-		counter++;
-
-		if(counter==tmp.length){
-
-		    var html_insert="";
-		    for( var l=0; l < tmp.length; l++){
-			html_insert+=tmp[l];
-		    }
-
-		    log_output.innerHTML= html_insert;
-		    updating=false;
-
-		}
-	    });
-
-	});
-
-
-    });
-
-}
-
