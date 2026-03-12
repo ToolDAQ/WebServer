d4c9f8edc7b8b1ec2c3f35274faf47c3c4e37693
marcus <root@dbServermoflaher@warwick.ac.uk>
2026-03-12 14:56:19 +0000

    minor updates to SetupDatabase, add postgresql.conf to track tuned parameters

--- SetupDatabase.sh
+++ SetupDatabase.sh
@@ -18,7 +18,7 @@ export PGDATA=${PGROOT}/data
 
 # only take action on first run
 if [ -f /.DBSetupDone ]; then
-
+	
 	# systemd version for baremetal
 	if [ ${USE_SYSTEMD} -eq 0 ]; then
 		# note no [ ] in following check
@@ -39,15 +39,40 @@ if [ -f /.DBSetupDone ]; then
 	fi
 	exit 0;
 fi
+
 export LC_ALL=C
-echo "Initialising postgresql cluster"
+echo "Initialising postgresql cluster at location ${PGROOT}. Is this OK?"
+select result in OK Change Cancel; do
+	case $result in
+		OK)
+			break;
+			;;
+		Change)
+			read -p "new path: " PGROOT
+			if [ $? -ne 0 ]; then
+				exit 0;
+			fi
+			# this cannot be the correct way to do this...
+			echo -e "Initialising postgresql cluster at location ${PGROOT}. Is this OK?\n1) OK\n2) Change\n3) Cancel"
+			;;
+		Cancel)
+			exit 0;
+			;;
+		*)
+			echo "enter 1, 2 or 3"
+			;;
+	esac
+done
+
 if [ ! -d ${PGROOT} ]; then
 	mkdir -p ${PGROOT}
 fi
 chown -R postgres:postgres ${PGROOT}
 cd ${PGROOT}
+# FIXME
 # --waldir=/todo/replication
-sudo -u postgres /usr/bin/initdb --data-checksums ${PGDATA}
+# locale='C' for faster string matching
+sudo -u postgres /usr/bin/initdb --data-checksums  --locale='C' ${PGDATA}
 
 # set it up to listen on all network interfaces, rather than (by default) localhost only
 echo "listen_addresses = '*'" | sudo -u postgres tee -a ${PGDATA}/postgresql.conf
@@ -74,7 +99,7 @@ fi
 echo "creating root database user"
 sudo -u postgres createuser -s root
 echo "creating 'daq' database"
-sudo -u postgres psql -c "create database daq with owner=root;"
+sudo -u postgres psql -c "CREATE DATABASE daq WITH owner=root;"
 
 # set timezone to UTC
 psql -ddaq -c "ALTER DATABASE daq SET TIME ZONE 'UTC';"
@@ -83,86 +108,129 @@ psql -ddaq -c "ALTER DATABASE daq SET TIME ZONE 'UTC';"
 # it's advisable to arrange columns in the table definition from largest to smallest data type.
 
 echo "creating users table"
-psql -ddaq -c "create table users (user_id serial NOT NULL, username text NOT NULL, password_hash text NOT NULL, permissions jsonb, UNIQUE (user_id));"
+psql -ddaq -c "CREATE TABLE users (user_id serial PRIMARY KEY, username text NOT NULL, password_hash text NOT NULL, permissions jsonb);"
+
+echo "creating index on users table"
+psql -ddaq -c "CREATE UNIQUE INDEX user_name_idx ON users(LOWER(username) text_pattern_ops);"
+
+echo "creating base_config table"
+psql -ddaq -c "CREATE TABLE base_config (config_id serial PRIMARY KEY, time timestamp with time zone NOT NULL DEFAULT now(), name text NOT NULL, version int NOT NULL, description text NOT NULL, author text NOT NULL, data json NOT NULL);"
+
+ # n.b. this index is doing double duty of enforcing unique {device:version} constraint and providing an ordered index
+ # a unique(name,version) constraint uses an index to accomplish this under the hood, by doing it explicitly we can make the index ordered
+echo "creating index on base_config table"
+psql -ddaq -c "CREATE UNIQUE INDEX ON base_config (name, version DESC NULLS LAST)"
+
+echo "creating autoincrement function for base_config version"
+psql -ddaq -c 'CREATE OR REPLACE FUNCTION "fn_base_config_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from base_config where name=new.name); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
+psql -ddaq -c 'CREATE TRIGGER trig_base_config_ver BEFORE insert ON base_config FOR EACH ROW EXECUTE PROCEDURE fn_base_config_ver();'
+
+echo "creating runmode_config table"
+psql -ddaq -c "CREATE TABLE runmode_config (config_id serial PRIMARY KEY, time timestamp with time zone NOT NULL DEFAULT now(), name text NOT NULL, version int NOT NULL, description text NOT NULL, author text NOT NULL, data json NOT NULL);"
+
+echo "creating index on runmode_config table"
+psql -ddaq -c "CREATE UNIQUE INDEX ON runmode_config (name, version DESC NULLS LAST)"
+
+echo "creating autoincrement function for runmode_config version"
+psql -ddaq -c 'CREATE OR REPLACE FUNCTION "fn_runmode_config_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from runmode_config where name=new.name); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
+psql -ddaq -c 'CREATE TRIGGER trig_runmode_config_ver BEFORE insert ON runmode_config FOR EACH ROW EXECUTE PROCEDURE fn_runmode_config_ver();'
 
 echo "creating run_info table"
-psql -ddaq -c "create table run_info (run int NOT NULL, subrun int NOT NULL, start_time timestamp with time zone NOT NULL, stop_time timestamp with time zone, config_id int NOT NULL, comments text NOT NULL, UNIQUE (run, subrun));"
-
-echo "creating index on run_info table"
-psql -ddaq -c "CREATE INDEX ON run_info (run DESC NULLS LAST)"
-
-echo "creating run_config table"
-psql -ddaq -c "create table run_config (config_id serial NOT NULL primary key, time timestamp with time zone NOT NULL DEFAULT now(), name text NOT NULL, version int NOT NULL, description text NOT NULL, author text NOT NULL, data jsonb NOT NULL, UNIQUE (name, version) );"
-
-echo "creating autoincrement function for run_config version"
-psql -ddaq -c 'create or replace function "fn_config_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from run_config where name=new.name); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
-psql -ddaq -c 'CREATE TRIGGER trig_config_ver BEFORE insert ON run_config FOR EACH ROW EXECUTE PROCEDURE fn_config_ver();'
+psql -ddaq -c "CREATE TABLE run_info (run serial PRIMARY KEY, start_time timestamp with time zone NOT NULL, stop_time timestamp with time zone, base_config_id int NOT NULL references base_config(config_id), runmode_config_id int NOT NULL references runmode_config(config_id), testing boolean NOT NULL, comments text NOT NULL);"
 
 echo "creating devices table"
-psql -ddaq -c "CREATE TABLE devices (name text not null);"
-psql -ddaq -c "CREATE UNIQUE INDEX dev_name_idx ON devices(LOWER(name));"
+psql -ddaq -c "CREATE TABLE devices (name text NOT NULL, retired boolean NOT NULL DEFAULT FALSE);"
+
+# functional index to ensure no duplicates even ignoring case
+echo "creating index on devices table"
+psql -ddaq -c "CREATE UNIQUE INDEX dev_name_idx ON devices(LOWER(name) text_pattern_ops);"
 
 echo "creating device_config table"
-# TODO enable below to require device_configs reference a device in the devices table
-#psql -ddaq -c "create table device_config (time timestamp with time zone NOT NULL DEFAULT now(), device text references devices(name), version int NOT NULL, author text NOT NULL, description text NOT NULL, data jsonb NOT NULL, UNIQUE (device, version) );"
-psql -ddaq -c "create table device_config (time timestamp with time zone NOT NULL DEFAULT now(), device text NOT NULL, version int NOT NULL, author text NOT NULL, description text NOT NULL, data jsonb NOT NULL, UNIQUE (device, version) );"
+# XXX IMPORTANT: VERSION 0 OF ALL DEVICE CONFIGURATIONS SHOULD BE DEVICE OFF
+psql -ddaq -c "CREATE TABLE device_config (time timestamp with time zone NOT NULL DEFAULT now(), device text references devices(name), version int NOT NULL, author text NOT NULL, description text NOT NULL, data json NOT NULL);"
 
 echo "creating index on device_config table"
-psql -ddaq -c "CREATE INDEX ON device_config (device, version DESC NULLS LAST)"
+psql -ddaq -c "CREATE UNIQUE INDEX ON device_config (device, version DESC NULLS LAST)"
 
 echo "creating autoincrement function for device_config version"
-psql -ddaq -c 'create or replace function "fn_devconfig_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from device_config where device=new.device); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
+psql -ddaq -c 'CREATE OR REPLACE FUNCTION "fn_devconfig_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from device_config where device=new.device); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
 psql -ddaq -c 'CREATE TRIGGER trig_devconfig_ver BEFORE insert ON device_config FOR EACH ROW EXECUTE PROCEDURE fn_devconfig_ver();'
 
 echo "creating calibration table"
-psql -ddaq -c "create table calibration (time timestamp with time zone NOT NULL DEFAULT now(), name text NOT NULL, version int NOT NULL, description text NOT NULL, data jsonb NOT NULL, UNIQUE (name, version) );"
+psql -ddaq -c "CREATE TABLE calibration (time timestamp with time zone NOT NULL DEFAULT now(), name text NOT NULL, version int NOT NULL, description text NOT NULL, data json NOT NULL;"
 
 echo "creating index on calibration table"
-psql -ddaq -c "CREATE INDEX ON calibration (name, version DESC NULLS LAST)"
+psql -ddaq -c "CREATE UNIQUE INDEX ON calibration (name, version DESC NULLS LAST)"
 
 echo "creating autoincrement function for calibration version"
-psql -ddaq -c 'create or replace function "fn_calibration_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from calibration where name=new.name); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
+psql -ddaq -c 'CREATE OR REPLACE FUNCTION "fn_calibration_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from calibration where name=new.name); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
 psql -ddaq -c 'CREATE TRIGGER trig_calibration_ver BEFORE insert ON calibration FOR EACH ROW EXECUTE PROCEDURE fn_calibration_ver();'
 
 echo "creating logging table"
-psql -ddaq -c "create table logging (time timestamp with time zone NOT NULL DEFAULT now(), device text NOT NULL, severity integer NOT NULL, message text NOT NULL);"
+psql -ddaq -c "CREATE TABLE logging (time timestamp with time zone NOT NULL DEFAULT now(), device text NOT NULL, severity integer NOT NULL, message text NOT NULL, repeats integer NOT NULL DEFAULT 1);"
 
 echo "creating indices on logging device name and message severity"
-psql -ddaq -c "CREATE INDEX ON logging (device) WITH (deduplicate_items = on);"
+#psql -ddaq -c "CREATE INDEX ON logging (device) WITH (deduplicate_items = on);" # is this redundant with below?
 psql -ddaq -c "CREATE INDEX ON logging (device,severity) WITH (deduplicate_items = on);"
+psql -ddaq -c "CREATE INDEX ON logging USING BRIN(time);"
+# FIXME should we combine this as part of the composite index? logging (device, severity, brin(time)) ?
+#'ALTER TABLE logging ALTER COLUMN device SET STATISTICS 1000;' Default is 100, maximum is 10000.
 
 echo "creating monitoring table"
-psql -ddaq -c "create table monitoring (time timestamp with time zone NOT NULL DEFAULT now(), device text NOT NULL, subject text NOT NULL, data jsonb NOT NULL);"
+psql -ddaq -c "CREATE TABLE monitoring (time timestamp with time zone NOT NULL DEFAULT now(), device text NOT NULL, subject text NOT NULL, data json NOT NULL);"
 
 echo "creating indices on monitoring device name and subject"
-psql -ddaq -c "CREATE INDEX ON monitoring (device) WITH (deduplicate_items = on);"
+#psql -ddaq -c "CREATE INDEX ON monitoring (device) WITH (deduplicate_items = on);"   # redundant?
 psql -ddaq -c "CREATE INDEX ON monitoring (device, subject) WITH (deduplicate_items = on);"
+psql -ddaq -c "CREATE INDEX ON monitoring USING BRIN(time);"
+
+# note on device name index usage: postgres never uses indexes for case insensitive searches!
+# we can create a functional index with 'lower(device)', but this only gets used if queries
+# also use 'select lower(device) ... where lower(device) = ...'
+# Perhaps better to just enforce that all device names is lowercase? postgres doesn't have a nice way to do this....
+# we could use 'CHECK lower(device)=device' on device table to ensure names are lowercase, but not sure it helps..
+#
+# also, we may need to specify 'text_pattern_ops' after the field name for text pattern matching to use an index!
+# https://www.www-old.bartlettpublishing.com/site/bartpub/blog/3/entry/329
+# only if the locale is not 'C' - probbly not default. use `show lc_collate;` to find out.
+# use '--no-locale' to initdb to set
+# https://www.postgresql.org/docs/current/indexes-opclass.html
+# N.B. B-tree indexes are only used for left-anchored searches: 'PMT%' not '%LED%'. Use GIN trigrams for latter.
+
 
 echo "creating alarms table"
-psql -ddaq -c "create table alarms (time timestamp with time zone NOT NULL DEFAULT now(), device text NOT NULL, level integer NOT NULL, alarm text NOT NULL, silenced integer DEFAULT 0 );"
+psql -ddaq -c "CREATE TABLE alarms (time timestamp with time zone NOT NULL DEFAULT now(), device text NOT NULL, critical BOOLEAN NOT NULL, alarm text NOT NULL, silenced integer DEFAULT 0 );"
+
+psql -ddaq -c "CREATE INDEX ON alarms (device) WITH (deduplicate_items = on);"
+psql -ddaq -c "CREATE INDEX ON alarms USING BRIN(time);"
 
 echo "creating rootplots table"
-psql -ddaq -c "create table rootplots (time timestamp with time zone NOT NULL DEFAULT now(), name text NOT NULL, version int NOT NULL, data jsonb NOT NULL, draw_options text NOT NULL DEFAULT '', lifetime int NOT NULL DEFAULT 5, UNIQUE (name, version));"
+psql -ddaq -c "CREATE TABLE rootplots (time timestamp with time zone NOT NULL DEFAULT now(), name text NOT NULL, version int NOT NULL, data json NOT NULL, draw_options text NOT NULL DEFAULT '', lifetime int NOT NULL DEFAULT 5);"
+
+psql -ddaq -c "CREATE UNIQUE INDEX ON rootplots (name, version DESC NULLS LAST)"
 
 echo "creating autoincrement function for rootplots version"
-psql -ddaq -c 'create or replace function "fn_rootplot_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from rootplots where name=new.name); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
+psql -ddaq -c 'CREATE OR REPLACE FUNCTION "fn_rootplot_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from rootplots where name=new.name); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
 psql -ddaq -c 'CREATE TRIGGER trig_rootplot_ver BEFORE insert ON rootplots FOR EACH ROW EXECUTE PROCEDURE fn_rootplot_ver();'
 
 echo "creating plotlyplots table"
-psql -ddaq -c "create table plotlyplots (time timestamp with time zone NOT NULL DEFAULT now(), name text NOT NULL, version int NOT NULL, data jsonb NOT NULL, layout jsonb NOT NULL DEFAULT '{}', lifetime int NOT NULL DEFAULT 5, UNIQUE (name, version));"
+psql -ddaq -c "CREATE TABLE plotlyplots (time timestamp with time zone NOT NULL DEFAULT now(), name text NOT NULL, version int NOT NULL, data json NOT NULL, layout json NOT NULL DEFAULT '{}', lifetime int NOT NULL DEFAULT 5);"
+
+psql -ddaq -c "CREATE UNIQUE INDEX ON plotlyplots (name, version DESC NULLS LAST)"
 
 echo "creating autoincrement function for plotlyplots version"
-psql -ddaq -c 'create or replace function "fn_plotlyplot_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from plotlyplots where name=new.name); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
+psql -ddaq -c 'CREATE OR REPLACE FUNCTION "fn_plotlyplot_ver"() returns "pg_catalog"."trigger" as $BODY$ begin new.version = (select COALESCE(MAX(version)+1,0) from plotlyplots where name=new.name); return NEW; end; $BODY$ LANGUAGE plpgsql VOLATILE COST 100;'
 psql -ddaq -c 'CREATE TRIGGER trig_plotlyplot_ver BEFORE insert ON plotlyplots FOR EACH ROW EXECUTE PROCEDURE fn_plotlyplot_ver();'
 
 echo "creating event_display table"
-psql -ddaq -c "create table event_display (time timestamp with time zone NOT NULL DEFAULT now(), evnt bigint NOT NULL, data jsonb NOT NULL, UNIQUE (evnt));"
+psql -ddaq -c "CREATE TABLE event_display (event_number bigint NOT NULL PRIMARY KEY, time timestamp with time zone NOT NULL DEFAULT now(), event_type integer NOT NULL, data json NOT NULL);"
 
-psql -ddaq -c "CREATE INDEX ON event_display (evnt DESC NULLS LAST)"
+echo "creating command_log table"
+psql -ddaq -c "CREATE TABLE command_log (time timestamp with time zone NOT NULL DEFAULT now(), user_id integer references users(user_id), command json NOT NULL)"
 
 echo "creating pmt table"
-psql -ddaq -c "create type pmt_location as enum ('bottom', 'barrel', 'top');"
-psql -ddaq -c "create table pmt (id int NOT NULL, x real NOT NULL, y real NOT NULL, z real, type text NOT NULL, size real NOT NULL, location pmt_location NOT NULL, UNIQUE (id));"
+psql -ddaq -c "CREATE type pmt_location as enum ('bottom', 'barrel', 'top');"
+psql -ddaq -c "CREATE TABLE pmt (id int PRIMARY KEY, x real NOT NULL, y real NOT NULL, z real, type text NOT NULL, size real NOT NULL, location pmt_location NOT NULL);"
 
 #echo "registering database to start on boot"
 #echo " sudo -u postgres /usr/bin/pg_ctl start -D ${PGDATA} -s -o \"-p 5432\" -w -t 300;" >> /etc/rc.local
@@ -172,7 +240,7 @@ echo "Inserting a default user"
 psql -ddaq -c "INSERT INTO users (username, password_hash) VALUES ('dev_user', 'c20cc404fe15337ce6d8a5b782576d9a21de03f8707065c8ccf7abb1cc939801');"
 
 echo "Inserting example monitoring data"
-psql -ddaq -c "INSERT INTO monitoring (time, device, subject, data) SELECT now() - (i * INTERVAL '1 minute') AS time, 'test_device' AS device, 'general' AS subject, jsonb_build_object( 'temperature', round((random() * 50 + 10)::numeric, 2), 'humidity', round((random() * 100)::numeric, 2)) AS data FROM generate_series(1, 100) i;"
+psql -ddaq -c "INSERT INTO monitoring (time, device, subject, data) SELECT now() - (i * INTERVAL '1 minute') AS time, 'test_device' AS device, 'general' AS subject, json_build_object( 'temperature', round((random() * 50 + 10)::numeric, 2), 'humidity', round((random() * 100)::numeric, 2)) AS data FROM generate_series(1, 100) i;"
 
 echo "Inserting example plotyplot data"
 psql -ddaq -c "INSERT INTO plotlyplots (name, time, version, data, layout) VALUES ('plotly1', 'now()', 1, '[{\"x\": [1, 2, 3, 4, 5], \"y\": [10, 20, 15, 30, 25], \"type\": \"scatter\"}]', '{\"title\": \"plotly1\"}');"
